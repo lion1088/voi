201da0cd8bdaf9b34e6126b76a9bbf902bf154d9
lion1088 <81590236+lion1088@users.noreply.github.com>
2022-02-06 18:19:49 +0000

    Add files via upload

---
+++ anekd.py
@@ -0,0 +1,6 @@
+import vv
+import os
+from tkinter import *
+
+Screen = Tk()
+Screen.title("Language Translator with GUI by- TechVidvan")
---
+++ map.py
@@ -0,0 +1,23 @@
+import vv
+import webbrowser
+#import map
+
+
+def kar():
+    cur = vv.voice[20:]
+    cur = str(cur)
+    print(cur)
+    
+    # cu = vv.voice[]
+    url = (f'https://www.google.com/maps/search/ + {cur}' )
+    webbrowser.open_new_tab(url)
+    # res = re.get(url)
+
+
+    
+
+        
+
+
+
+    
